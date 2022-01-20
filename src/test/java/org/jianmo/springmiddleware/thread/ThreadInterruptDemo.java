17073301e7690ff74d861363fa89aaaee777fc2a
jianmo1997 <1183508648@qq.com>
2022-01-20 22:51:43 +0800

    Thread.interrupt

---
+++ src/test/java/org/jianmo/springmiddleware/thread/ThreadInterruptDemo.java
@@ -0,0 +1,33 @@
+package org.jianmo.springmiddleware.thread;
+
+/**
+ * 线程中断测试类
+ *
+ *
+ */
+public class ThreadInterruptDemo{
+
+  public static void main(String[] args) {
+    Thread thread = new Thread(() ->{
+
+//      while (Thread.interrupted()) {
+        System.out.println(System.currentTimeMillis());
+//        try {
+//          System.out.println("开始睡眠");
+//          Thread.sleep(5000);
+//          System.out.println("结束睡眠");
+//        } catch (InterruptedException e) {
+//          e.printStackTrace();
+//        }
+//        System.out.println(System.currentTimeMillis());
+//      }
+
+    });
+
+    thread.start();
+    thread.interrupt();
+
+  }
+
+
+}
